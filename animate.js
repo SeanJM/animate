16c6d17cc64aa01942f3b1a749556b08d18f7b81
SeanJM <seanjmacisaac@gmail.com>
2014-09-17 21:36:31 -0400

    - Fixed a few bugs! - Now, the event trigger can work for both an animation and a transition

--- animate.js
+++ animate.js
@@ -2,7 +2,7 @@
 
 /* 
   Name   : Animate
-  Version: 1.2.1
+  Version: 1.2.4
   License: MIT License
   By     : Sean MacIsaac
 */
@@ -12,7 +12,7 @@
 ;function animate() {
   var options = {
     'class' : '_animated',
-    'start'    : '-in',
+    'start' : '-in',
     'end'   : '-out'
   }
   var arr = Array.prototype.slice.apply(arguments);
@@ -23,50 +23,48 @@
       $.extend(options,arr[i]);
     }
   }
-  function getCssProperty(el,property) {
-    var arr     = ['','ms','webkit','Moz','O'];
-    var style   = window.getComputedStyle(el[0]);
-    var r;
-    function capitalize(str) {
-      return str[0].toUpperCase()+str.substr(1,str.length-1);
-    }
-    if (style !== null) {
-      for (var i=0;i < arr.length;i++) {
-        if (arr[i].length < 1) {
-          r = property;
-        } else {
-          r = arr[i]+capitalize(property);
-        }
-        if (typeof style[r] === 'string') {
-          return style[r];
-        }
+  function matchArray(arr,string) {
+    var out = false;
+    for (var i = 0;i < arr.length;i++) {
+      if (arr[i].match(new RegExp(string,'i')) !== null) {
+        out = true;
       }
     }
-    return false;
+    return out;
   }
-  function jsTime(style) {
-    if (style) {
-      return parseFloat(style.match(/([0-9]+(\.[0-9]+|))s/)[1])*1000;
+  function endName(el) {
+    var arr = [];
+    // from bootstrap (ish)
+    var eventNames = {
+      'WebkitTransition' : 'webkitTransitionEnd',
+      'MozTransition'    : 'transitionend',
+      'OTransition'      : 'oTransitionEnd otransitionend',
+      'transition'       : 'transitionend',
+      'WebkitAnimation'  : 'webkitAnimationEnd',
+      'MozAnimation'     : 'animationend',
+      'OAnimation'       : 'oAnimationEnd',
+      'animation'        : 'animationend',
+    }
+    for (var name in eventNames) {
+      if (typeof el[0].style[name] !== 'undefined' && !matchArray(arr,eventNames[name])) {
+        arr.push(eventNames[name]);
+      }
+    }
+    if (arr.length) {
+      return arr;
     } else {
-      return 0;
+      return false;
     }
   }
-  function getTime(el) {
-    var obj = {
-      duration : 0,
-      delay    : 0
-    };
-    // For IE 8
-    if (typeof window.getComputedStyle === 'function') {
-      obj.duration  = jsTime(getCssProperty(el,'transitionDuration'));
-      obj.delay     = jsTime(getCssProperty(el,'transitionDelay'));
-
-      if (obj.delay === 0 && obj.duration === 0) {
-        obj.duration  = jsTime(getCssProperty(el,'animationDuration'));
-        obj.delay     = jsTime(getCssProperty(el,'animationDelay'));
+  function trigger(el,callback) {
+    var end = endName(el);
+    if (end) {
+      for (var i = 0;i<end.length;i++) {
+        el[0].addEventListener(end[i], callback, false);
       }
+    } else {
+      callback();
     }
-    return obj.duration+obj.delay;
   }
   function init() {
     // Remove the nesting of the arguments from being passed inside an array
@@ -76,7 +74,7 @@
       if (typeof k === 'function') {
         options.callback = k;
       } else if (typeof k === 'object') {
-        el = $.extend(options,k);
+        el = k;
       }
     });
     function toggle() {
@@ -87,14 +85,15 @@
         el.addClass(options['class'] + options['end']);
         el.removeClass(options['class'] + options['start']);
       }
-      setTimeout(function () {
+      // 
+      trigger(el,function () {
         if (options.direction === 'out') {
           el.removeClass(options['class'] + options['end']);
         }
         if (typeof options.callback === 'function') {
           options.callback(el);
         }
-      },getTime(el));
+      });
     }
     if (typeof el[0] === 'undefined') {
       return false;
@@ -104,10 +103,10 @@
     }
   }
   return {
-    animatedIn: function () {
+    animatedIn: function (el) {
       return (el.hasClass(options['class'] + options['start']));
     },
-    animatedOut: function () {
+    animatedOut: function (el) {
       return (el.hasClass(options['class'] + options['end']));
     },
     start: function () {
