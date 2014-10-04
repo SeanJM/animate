934e6235a82c39ec5ed030efe4917d900529f42e
SeanJM <seanjmacisaac@gmail.com>
2014-10-04 10:01:20 -0400

    bug fixes and changes to how animate().scroll() works

--- animate.js
+++ animate.js
@@ -32,19 +32,9 @@
     }
     return out;
   }
-  function endName(el) {
+  function getName(el,eventNames) {
     var arr = [];
     // from bootstrap (ish)
-    var eventNames = {
-      'WebkitTransition' : 'webkitTransitionEnd',
-      'MozTransition'    : 'transitionend',
-      'OTransition'      : 'oTransitionEnd otransitionend',
-      'transition'       : 'transitionend',
-      'WebkitAnimation'  : 'webkitAnimationEnd',
-      'MozAnimation'     : 'animationend',
-      'OAnimation'       : 'oAnimationEnd',
-      'animation'        : 'animationend',
-    }
     for (var name in eventNames) {
       if (typeof el[0].style[name] !== 'undefined' && !matchArray(arr,eventNames[name])) {
         arr.push(eventNames[name]);
@@ -57,49 +47,50 @@
     }
   }
   function trigger(el,callback) {
-    var end = endName(el);
+    var end = getName(el,{
+      'WebkitTransition' : 'webkitTransitionEnd',
+      'MozTransition'    : 'transitionend',
+      'OTransition'      : 'oTransitionEnd',
+      'transition'       : 'transitionend',
+      'WebkitAnimation'  : 'webkitAnimationEnd',
+      'MozAnimation'     : 'animationend',
+      'OAnimation'       : 'oAnimationEnd',
+      'animation'        : 'animationend',
+    });
     if (end) {
       for (var i = 0;i<end.length;i++) {
+        el[0].removeEventListener(end[i], callback, false);
         el[0].addEventListener(end[i], callback, false);
       }
     } else {
       callback();
     }
   }
-  function init() {
+  function init(el,direction,callback) {
     // Remove the nesting of the arguments from being passed inside an array
-    var arr = Array.prototype.slice.apply(arguments[0]);
-    var el;
-    $.each(arr,function (i,k) {
-      if (typeof k === 'function') {
-        options.callback = k;
-      } else if (typeof k === 'object') {
-        el = k;
-      }
-    });
-    function toggle() {
-      if (options['direction'] === 'in') {
+    function toggle(el) {
+      if (direction === 'in') {
         el.removeClass(options['class'] + options['end']);
         el.addClass(options['class'] + options['start']);
-      } else {
+      } else if (el.hasClass(options['class'] + options['start'])) {
         el.addClass(options['class'] + options['end']);
         el.removeClass(options['class'] + options['start']);
       }
       // 
       trigger(el,function () {
-        if (options.direction === 'out') {
+        if (direction === 'out') {
           el.removeClass(options['class'] + options['end']);
         }
-        if (typeof options.callback === 'function') {
-          options.callback(el);
+        if (typeof callback === 'function') {
+          callback(el);
         }
       });
+      return el;
     }
-    if (typeof el[0] === 'undefined') {
+    if (!el.size()) {
       return false;
     } else {
-      toggle();
-      return el;
+      return toggle(el);
     }
   }
   return {
@@ -109,30 +100,37 @@
     animatedOut: function (el) {
       return (el.hasClass(options['class'] + options['end']));
     },
-    start: function () {
-      options['direction'] = 'in';
-      return init(arguments);
+    start: function (el,callback) {
+      return init(el,'in',callback);
     },
-    end: function () {
-      options['direction'] = 'out';
-      return init(arguments);
+    end: function (el,callback) {
+      return init(el,'out',callback);
     },
-    scroll: function (el) {
+    scroll: function (parent,pos) {
       var time   = 70;
-      var pos    = (el.offset().top-el.height()/2)-($(window).height()/2);
-      var start  = window.pageYOffset;
+      var start;
       var i      = 0;
       var frames = 20;
+      var pos;
+
+      if (parent === window) {
+        start = window.pageYOffset
+      } else {
+        start = parent.offset().top;
+      }
 
       function s() {
         i++;
-        window.scrollTo(0,(start-((start/frames)*i))+((pos/frames)*i));
+        parent.scrollTo(0,(start-((start/frames)*i))+((pos/frames)*i));
         if (i<frames) {
           setTimeout(function () {
             s();
           },(time/frames));
         }
       };
+      if (parent !== window) {
+        parent = parent[0];
+      }
       s();
     }
   }
